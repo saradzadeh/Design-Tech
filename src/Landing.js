821cafd4fb68bd152c9d57f6dd2cc65ff27ab67f
saradzadeh <sara.daghaghzadeh2@gmail.com>
2022-08-23 19:02:52 +1000

    design button

---
+++ src/Landing.js
@@ -0,0 +1,17 @@
+import './Landing.css';
+import React, { Component } from 'react'
+
+
+export default function App(){
+    return (
+      <div>
+          <div className = "button">
+            Read More 
+          </div>
+          <div className = "button">
+              Submit your Application
+          </div>
+        
+      </div>
+    )
+}
